2a12e7958c9b54304fa6d41c761d923d1fb01304
Subhashis Sahu <sahusubhashis01@gmail.com>
2026-01-30 17:13:48 +0530

    add alert

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Contact() {
+  const [message, setMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,27 +20,50 @@ function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("https://subhashissahu.onrender.com/api/public/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+    const res = await fetch(
+      "https://subhashissahu.onrender.com/api/public/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
       },
-      body: JSON.stringify(formData),
-    });
+    );
 
     if (res.ok) {
-      alert("Message sent successfully ✅");
+      setAlertType("Success");
+
+      setMessage("Message sent Succesfully");
+      setshowAlert(true);
       setFormData({ name: "", email: "", message: "" });
     } else {
-      alert("Something went wrong ❌");
+      setAlertType("error");
+      setMessage("Something went wrong");
+      setshowAlert(true);
     }
   };
+  const [showAlert, setshowAlert] = useState(false);
+  useEffect(() => {
+    if (!showAlert) return;
+
+    const timer = setTimeout(() => {
+      setshowAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [showAlert]);
 
   return (
-    <div id="contact" className="px-10 py-20 grid grid-cols-1 md:grid-cols-2 gap-10 items-center scroll-mt-20">
+    <div
+      id="contact"
+      className="px-10 py-20 grid grid-cols-1 md:grid-cols-2 gap-10 items-center scroll-mt-20"
+    >
       <div>
         <h3 className="text-3xl font-semibold mb-4">Got a project in mind?</h3>
-        <p className="text-gray-400">Let’s work together to create something amazing.</p>
+        <p className="text-gray-400">
+          Let’s work together to create something amazing.
+        </p>
       </div>
 
       <form
@@ -77,6 +103,19 @@ function Contact() {
           Send message
         </button>
       </form>
+      {showAlert && (
+        <div
+          className={`fixed bottom-5 right-5 ${alertType == "Success" ? "bg-green-600" : "bg-red-600"}  text-white px-4 py-3 rounded-lg shadow-lg`}
+        >
+          {message}
+          <button
+            className="ml-3 text-sm underline"
+            onClick={() => setshowAlert(false)}
+          >
+            Close
+          </button>
+        </div>
+      )}
     </div>
   );
 }
